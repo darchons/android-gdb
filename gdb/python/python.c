ad71473eab29a336dd3cbccac7049428aeb2620a
Jim Chen <nchen@mozilla.com>
2013-03-14 11:17:25 -0400

    Let Python find itself

--- gdb/python/python.c
+++ gdb/python/python.c
@@ -1216,7 +1216,7 @@ message == an error message without a stack will be printed."),
 			&user_show_python_list);
 
 #ifdef HAVE_PYTHON
-#ifdef WITH_PYTHON_PATH
+#if 0 // #ifdef WITH_PYTHON_PATH
   /* Work around problem where python gets confused about where it is,
      and then can't find its libraries, etc.
      NOTE: Python assumes the following layout:
